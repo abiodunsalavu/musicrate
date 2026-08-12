a6f5a992aef8b8c73b000e1205f29dbc7ed53992
Abiodun Salavu <asalavu56@gmail.com>
2026-08-12 21:49:23 +0100

    Changing the meta data for the page title

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+
 export default function Home() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
