926ff1dbf396823c6158431d4022d3fb2f8d6796
Abiodun Salavu <asalavu56@gmail.com>
2026-08-12 18:34:03 +0100

    adding my collection button

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-
 export default function Home() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
